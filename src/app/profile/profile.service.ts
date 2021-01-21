d90b3fbeeb7b3976c519e1f391def31633e82717
Sathiya Prakash <Sathiya.Prakash@gds.ey.com>
2021-01-22 00:19:16 +0530

    initial

---
+++ src/app/profile/profile.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+}
